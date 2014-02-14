072b0d314e9dac19f606718a110dbbc02a1083c0
Rhynoo <alexandre.loubier@gmail.com>
2014-02-14 17:36:26 +0100

    test cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,19 @@
 #include <SDL\SDL.h>
 
 #include "App.h"
+#include <iostream>
+using namespace std;
 
 int main ( int argc, char** argv )
 {
+	cout << "Game Started" << endl;
     SDL_Surface* screen = App_Init();
 
     App_Run(screen);
 
     App_End(screen);
 
-    printf("Game Ended\n");
+    cout << "Game Ended" << endl;
     return 0;
 }
 
